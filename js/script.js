ad42a6eb30715edf1dff42a23213f863a1ef9400
Brijesh Bittu <brijeshb42@gmail.com>
2016-03-14 01:23:21 +0530

    Disqus engage

--- js/script.js
+++ js/script.js
@@ -14,10 +14,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
       if(disqus_shortname === ''){
           disqus_shortname = '{{ site.disqus }}';
           dt.innerHTML = 'Loading comments...';
+          var disqus_config = function () {
+              this.page.url = window.APP.PAGE_URL;
+              this.page.identifier = window.APP.PAGE_IDENTIFIER;
+          };
           (function() {
               var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
               dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-              (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+              (document.head || document.body).appendChild(dsq);
           })();
       }
       if(commentHidden){
